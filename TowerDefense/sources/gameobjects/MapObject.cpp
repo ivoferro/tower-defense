11773f7b1e96676730b6690500b6da0fb449a5ca
Pedro Fernandes <pedro.n.f.fernandes@gmail.com>
2017-12-01 13:03:26 +0000

    add MapObject class + map resources

---
+++ TowerDefense/sources/gameobjects/MapObject.cpp
@@ -0,0 +1,43 @@
+#include "../../headers/components/Transform.h"
+#include "../../headers/gameobjects/MapObject.h"
+#include "../../headers/objloader/glm.h"
+
+
+MapObject::MapObject(char * path)
+{
+	this->path = path;
+
+	GLboolean	isInit;
+	StudioModel   mdl;   // MDL Model
+
+	addComponent("transform", new Transform());
+}
+
+
+MapObject::~MapObject()
+{
+}
+
+void MapObject::draw()
+{
+	initMapObject(path);
+
+	Transform * t = (Transform*)getComponentById("transform");
+
+	glPushMatrix();
+	
+	glTranslatef(t->position->x, t->position->y, t->position->z);
+	glScalef(t->scale->x, t->scale->y, t->scale->z);
+	
+	mdlviewer_display(mdl);
+	glPopMatrix();
+}
+
+void MapObject::initMapObject(char * path)
+{
+	if (isInit == GL_FALSE)
+	{
+		mdlviewer_init(path, mdl);
+		isInit = GL_TRUE;
+	}
+}
